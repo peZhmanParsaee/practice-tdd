f326e16c1688f9c31facd9656b030a523476b6d9
pezhmanparsaee <p.parsaee@gmail.com>
2020-09-28 17:40:24 +0330

    remove duplication in orderTotal by adding sumOrderTotal inner function

--- order-total.js
+++ order-total.js
@@ -1,4 +1,10 @@
 function orderTotal(fetch, config, order) {
+  const sumOrderItems = (order) =>
+    order.items.reduce(
+      (prev, cur) => cur.price * (cur.quantity || 1) + prev,
+      0
+    );
+
   if (order.country) {
     return fetch(
       "https://api.vatsense.com/1.0/rates?country_code=" + order.country,
@@ -11,19 +17,10 @@ function orderTotal(fetch, config, order) {
     )
       .then((response) => response.json())
       .then((result) => result.data.standard.rate)
-      .then(
-        (vat) =>
-          order.items.reduce(
-            (prev, cur) => cur.price * (cur.quantity || 1) + prev,
-            0
-          ) *
-          (vat / 100 + 1)
-      );
+      .then((vat) => sumOrderItems(order) * (vat / 100 + 1));
   }
 
-  return Promise.resolve(
-    order.items.reduce((prev, cur) => cur.price * (cur.quantity || 1) + prev, 0)
-  );
+  return Promise.resolve(sumOrderItems(order));
 }
 
 module.exports = orderTotal;
